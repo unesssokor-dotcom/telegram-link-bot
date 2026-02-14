d335999f84dd7b84e0fed5788c7cd5898db395fa
unesssokor-dotcom <unesssokor@gmail.com>
2026-02-14 18:34:56 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,52 @@
+import telebot
+import json
+import os
+
+TOKEN = os.environ.get("8454099300:AAEepTp2iinHGrl2YYLQNO6_1u18rzCKmsg")
+bot = telebot.TeleBot(TOKEN)
+
+DATA_FILE = "products.json"
+
+if not os.path.exists(DATA_FILE):
+    with open(DATA_FILE, "w") as f:
+        json.dump({}, f)
+
+def load_data():
+    with open(DATA_FILE, "r") as f:
+        return json.load(f)
+
+def save_data(data):
+    with open(DATA_FILE, "w") as f:
+        json.dump(data, f)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Welcome ✅\nUse:\n/add name | link\n/get name")
+
+@bot.message_handler(commands=['add'])
+def add_product(message):
+    try:
+        text = message.text.replace("/add ", "")
+        name, link = text.split("|")
+        name = name.strip().lower()
+        link = link.strip()
+
+        data = load_data()
+        data[name] = link
+        save_data(data)
+
+        bot.reply_to(message, f"✅ Saved: {name}")
+    except:
+        bot.reply_to(message, "❌ Format غلط\nاستعمل:\n/add product name | link")
+
+@bot.message_handler(commands=['get'])
+def get_product(message):
+    name = message.text.replace("/get ", "").strip().lower()
+    data = load_data()
+
+    if name in data:
+        bot.reply_to(message, data[name])
+    else:
+        bot.reply_to(message, "❌ Product ما لقيتش")
+
+bot.infinity_polling()
